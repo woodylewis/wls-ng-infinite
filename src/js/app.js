deeb812bdb46740abd58413398bdf27156a5d454
woodylewis <woody@woodylewis.com>
2015-10-11 09:45:43 -0400

    Remove hashtag from urls

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,49 @@ angular.module('gothamlane', [
 	'ui.router',
 	'gothamlane.mainCtrl'
 ])
+.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  })
+  .hashPrefix('!');
+
+  $stateProvider
+    .state('main', {
+      url: "/main",
+      views: {
+        "state" : { templateUrl: "partials/main.html" }
+      }
+    })
+    .state('narration', {
+      url: "/narration",
+      views: {
+        "state" : { 
+                    templateUrl: "partials/narration.html" ,
+                    controller: function($scope, $sce, $anchorScroll, $location) {
+                      $scope.$parent.markup = $sce.trustAsHtml($scope.$parent.cn.body);
+                      $location.url($scope.$parent.cn.url);
+                      //$location.hash('top');
+                      //$anchorScroll();
+                    }
+            }
+        }
+    })
+    /*
+    .state('referral', {
+      url: "/:narrationUrl",
+      views: {
+        "state" : { 
+                    templateUrl: "partials/narration.html" ,
+                    controller: function ($scope, $filter, $stateParams) {
+                    var filtered = $filter('filter')($scope.posts, {url: $stateParams.narrationUrl});
+                    console.log('IN REFERRAL STATE CONTROLLER');
+                    $scope.parent.cn = filtered[0];
+                    }
+              }
+        }
+    })*/;
+}])
 .controller('appCtrl', ['$scope', '$state', function($scope, $state) {
   $scope.goHome = function () {
     $state.go('main');
--- src/js/controllers/mainCtrl.js
+++ src/js/controllers/mainCtrl.js
@@ -4,43 +4,6 @@ angular.module('gothamlane.mainCtrl', [
   'infinite-scroll',
   'gothamlane.narrationEngine'
 ])
-.config(['$stateProvider', function($stateProvider) {
-  $stateProvider
-    .state('main', {
-      url: "/main",
-      views: {
-        "state" : { templateUrl: "partials/main.html" }
-      }
-    })
-    .state('narration', {
-      url: "/narration",
-      views: {
-        "state" : { 
-                    templateUrl: "partials/narration.html" ,
-                    controller: function($scope, $sce, $anchorScroll, $location) {
-                      $scope.$parent.markup = $sce.trustAsHtml($scope.$parent.cn.body);
-                      $location.url($scope.$parent.cn.url);
-                      //$location.hash('top');
-                      //$anchorScroll();
-                    }
-            }
-        }
-    })
-    /*
-    .state('referral', {
-      url: "/:narrationUrl",
-      views: {
-        "state" : { 
-                    templateUrl: "partials/narration.html" ,
-                    controller: function ($scope, $filter, $stateParams) {
-                    var filtered = $filter('filter')($scope.posts, {url: $stateParams.narrationUrl});
-                    console.log('IN REFERRAL STATE CONTROLLER');
-                    $scope.parent.cn = filtered[0];
-                    }
-              }
-        }
-    })*/;
-}])
 .controller('mainCtrl', ['$scope', '$state', '$sce', '$location', '$anchorScroll', '$filter', 'NarrationEngine', 
   function($scope, $state, $sce, $location, $anchorScroll, $filter, NarrationEngine) {
     $scope.ne = new NarrationEngine();
